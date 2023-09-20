12450bacbaaa9af7de40814e6bc465a3fbfcf7e4
anaju555 <125681455+anaju555@users.noreply.github.com>
2023-09-20 13:12:16 -0300

    Update node.js

--- node.js
+++ node.js
@@ -2,5 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Ana Julia Reis de Sousa-20/09/2023');
+    var dataAtual = new Date();
+    var dataFormatada = dataAtual.toDateString();
+  res.end('Ana Julia Reis de Sousa!\nData atual: ' + dataFormatada);
 }).listen(8024);
